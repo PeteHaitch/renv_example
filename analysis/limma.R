e04978276bee8d02095f14097cfa5465a59aefb1
PeteHaitch <peter.hickey@gmail.com>
2021-02-23 12:41:09 +1100

    Initial commit

---
+++ analysis/limma.R
@@ -0,0 +1,21 @@
+library(limma)
+
+# Simulate gene expression data for 100 probes and 6 microarrays
+# Microarray are in two groups
+# First two probes are differentially expressed in second group
+# Std deviations vary between genes with prior df=4
+sd <- 0.3*sqrt(4/rchisq(100,df=4))
+y <- matrix(rnorm(100*6,sd=sd),100,6)
+rownames(y) <- paste("Gene",1:100)
+y[1:2,4:6] <- y[1:2,4:6] + 2
+design <- cbind(Grp1=1,Grp2vs1=c(0,0,0,1,1,1))
+options(digits=3)
+
+# Ordinary fit
+fit <- lmFit(y,design)
+fit <- eBayes(fit)
+topTable(fit,coef=2)
+dim(fit)
+colnames(fit)
+rownames(fit)[1:10]
+names(fit)
